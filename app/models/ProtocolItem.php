ed7285c234b0e20622d9e9a010d8455897e233a4
administrator <administrator@SHQAFE01.snm.local>
2020-04-16 21:11:41 -0300

    Second Commit NO Details

--- app/models/ProtocolItem.php
+++ app/models/ProtocolItem.php
@@ -2,23 +2,23 @@
 
 namespace app\models;
 
-class ProtocolItem extends TemplateModel{
+class ProtocolItem {
 
-    private $path;
+    const TABLE_NAME = 'protocol_item' ;
+
+    private $id_item;
     private $filename;
     private $purchase_order;
-    private $vendor;
+    private $vendor_name;
     private $issue_date;
     private $due_date;
-    private $invoice_num;
-    private $sent_date;
-
-
+    private $num_doc;
 
     function __construct(){
 
-        $this->sent_date = "01/01/2020";
     }
+
+    
 }
 
 ?>
---
+++ app/models/Protocolo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Protocolo;
+
+class Protocolo{
+
+    const TABLE_NAME = 'protocol_header';
+
+    /**
+     * @var int
+     */
+    private $id_protcol;
+
+    /**
+     * @var string
+     */
+    private $user_email;
+
+    /**
+     * @var DateTime
+     */
+    private $creation_date;
+
+    public function __construct(){
+        
+        
+    }
+
+}
+
+?>
